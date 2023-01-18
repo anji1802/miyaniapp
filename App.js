cb14823480fa29b7c21e4f25e8d8b37ddc427288
RajshahES <rajshah@exultitsolution.com>
2023-01-18 10:56:59 +0530

    my first project

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StatusBar, TextInput } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+export default function  App() {
+  function done(){
+    alert('completed');
+  }
+  {/*} const saygoodbye= () =>
+  {
+    alert('goodbye')
+  } */}
+  return(
+    <View style={styles.container}> 
+    <TextInput style={{fontSize:20}}>User Name</TextInput>
+    <TextInput style={{fontSize:20}}>Password</TextInput>
+     
+    
+    <Button title="Login" onPress={done} />
+    {/* <Button title="goodbye" onPress={saygoodbye} />*/}
+
+    
+
+    </View>
+  )
+ 
+} 
+      const styles = StyleSheet.create({
+      container: {
+         flex: 1,
+         backgroundColor: 'violet',
+         alignItems: 'center',
+         justifyContent: 'center',
+         padding: 20,
+         margin:20
+         
+   },
+
+
+   
+   
+});
